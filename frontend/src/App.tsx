84a05ebb35d3f9041702ba5182f5c0d84b6ac74e
Abishek Devendran <abishekdevendran@gmail.com>
2022-07-04 21:34:59 +0530

    Initial Commit resolving #1

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='justify-center w-screen text-center'>
+    Vite and TailwindCSS setup successfully.
+    </div>
+  )
+}
+
+export default App
