7e2f0a9ebf08c4c48025be16b49c2667dcd2cc26
Abishek Devendran <abishekdevendran@gmail.com>
2022-07-10 14:58:25 +0530

    heroku serve static 4

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const httpServer = createServer(app);
 const PORT = process.env.PORT || 5000;
 
-if (true) {
+if (process.env.NODE_ENV === "production") {
   console.log("production");
   app.use(express.static(path.join(__dirname, "frontend/dist")));
 }
