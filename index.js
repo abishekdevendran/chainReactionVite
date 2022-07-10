e8318e1ad657f36af161b5a6171af808b996209c
Abishek Devendran <63919062+abishekdevendran@users.noreply.github.com>
2022-07-10 16:08:44 +0530

    Catchall update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === "production") {
   console.log("production");
   app.use(express.static(path.join(__dirname, "frontend/dist")));
 }
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/dist"));
 });
 
